d3b685504b6691a3fb9a2de08cfcb902e2df893b
philter <hello@rive.app>
2026-09-11 16:15:12 +0000

    rive-cli 1.0.2

--- Casks/rive-cli.rb
+++ Casks/rive-cli.rb
@@ -1,7 +1,7 @@
 cask "rive-cli" do
-  version "1.0.1"
-  sha256 arm:          "6f95a2b11eea92bde4da7c6750cd381a0b557530f975adc22e80cac31b5c1bbf",
-         x86_64_linux: "a1d5c07209847cf3ee831a959cbf557afe0b6b412b940e0b46eabd3808376032"
+  version "1.0.2"
+  sha256 arm:          "1cb3c910d67c814cf03cd9c779bcc8b03ac145a5487d20a13ed27a1f37aa07ba",
+         x86_64_linux: "9c8adbabf7ae6457bc01ea7d75a1f145ba51a99b67a69d9ac2bdcc6cf5363710"
 
   on_macos do
     url "https://releases.rive.app/cli/v#{version}/rive-macos-arm64.tar.gz"
